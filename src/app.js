038c7c89704512bfb8330333759429770bf875db
tonaadefokun <84527335+tonaadefokun@users.noreply.github.com>
2021-07-23 22:13:26 +0100

    updated the time, to return timestamp

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
+function formatDate(timestamp) {
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sarturday",
+  ];
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let day = daysOfWeek[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data.main.temp);
   console.log(response.data.name);
   console.log(response.data.weather[0].description);
   console.log(response.data.main.humidity);
   console.log(response.data.wind.speed);
+  console.log(response.data.dt);
 
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#innerTemp").innerHTML = Math.round(
@@ -17,9 +42,12 @@ function displayTemperature(response) {
   document.querySelector(
     "#windSpeed"
   ).innerHTML = `Wind: ${response.data.wind.speed}Km/h`;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "5d28e41830862bc850144acfa82e7516";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
