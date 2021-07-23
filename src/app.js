b235b14660175ac9e33e1d16948b7b1680f39c61
tonaadefokun <84527335+tonaadefokun@users.noreply.github.com>
2021-07-23 22:31:05 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -43,6 +43,4 @@ function displayTemperature(response) {
 let city = "Toronto";
 let apiKey = "5d28e41830862bc850144acfa82e7516";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-
 axios.get(apiUrl).then(displayTemperature);
